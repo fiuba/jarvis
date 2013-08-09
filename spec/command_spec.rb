0c2153788a48a2480efd8f051b642935bfb65dfa
NicoPaez <nicopaez@gmail.com>
2013-08-09 19:29:07 -0300

    Tests fixed

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -6,19 +6,19 @@ describe Command do
 	describe 'execute' do
 
 		it 'should return true when system call returns 0' do
-			script_path = File.expand_path(File.dirname(__FILE__) + '/script_ok.sh')
+			script_path = File.expand_path(File.dirname(__FILE__) + '/resources/script_ok.sh')
 			cmd = Command.with_statement("bash #{script_path}")
 			cmd.execute.should be true
 		end
 
 		it 'should return false when system call returns 1' do
-			script_path = File.expand_path(File.dirname(__FILE__) + '/script_fail.sh')
+			script_path = File.expand_path(File.dirname(__FILE__) + '/resources/script_fail.sh')
 			cmd = Command.with_statement("bash #{script_path}")
 			cmd.execute.should be false
 		end
 
 		it 'should capture output' do
-			script_path = File.expand_path(File.dirname(__FILE__) + '/script_some_output.sh')
+			script_path = File.expand_path(File.dirname(__FILE__) + '/resources/script_some_output.sh')
 			cmd = Command.with_statement("bash #{script_path}")
 			cmd.execute
 			cmd.output.should eq "some_output\n"
